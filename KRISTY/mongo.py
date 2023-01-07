75dfdf42e8921cbfbae68284167dc4b5a9920919
AXEON-XD <99555142+AXEON-XD@users.noreply.github.com>
2023-01-07 19:45:22 +0530

    Update mongo.py

--- KRISTY/mongo.py
+++ KRISTY/mongo.py
@@ -2,7 +2,7 @@ from typing import Dict, Union
 
 from pymongo import MongoClient
 
-from FallenRobot import MONGO_DB_URI
+from KRISTY import MONGO_DB_URI
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
