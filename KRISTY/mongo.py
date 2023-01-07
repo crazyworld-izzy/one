8b266894a5dffce0cb7d63a127b5abc5981d1df2
AXEON-XD <99555142+AXEON-XD@users.noreply.github.com>
2023-01-07 19:14:42 +0530

    Update mongo.py

--- KRISTY/mongo.py
+++ KRISTY/mongo.py
@@ -1,24 +1,13 @@
-import asyncio
-import sys
+import inspect
+import re
+from pathlib import Path
 
-from motor import motor_asyncio
-from KRISTY import MONGO_DB_URI 
 from pymongo import MongoClient
-from pymongo.errors import ServerSelectionTimeoutError
-from KRISTY.confing import get_int_key, get_str_key
+from telethon import events
 
-
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "KRISTY"
-
+from KRISTY import MONGO_DB_URI, telethn
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
-db = motor[MONGO_DB]
+client = MongoClient(MONGO_DB_URI)
 db = client["KRISTY"]
-try:
-    asyncio.get_event_loop().run_until_complete(motor.server_info())
-except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+gbanned = db.gban
