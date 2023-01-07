74746dd6da147c6dda98520e1513c213f80fcd80
AXEON-XD <99555142+AXEON-XD@users.noreply.github.com>
2023-01-07 21:39:59 +0530

    Update mongo.py

--- KRISTY/mongo.py
+++ KRISTY/mongo.py
@@ -1,9 +1,13 @@
-from typing import Dict, Union
+import inspect
+import re
+from pathlib import Path
 
 from pymongo import MongoClient
+from telethon import events
 
-from KRISTY import MONGO_DB_URI
+from KRISTY import MONGO_DB_URI, telethn
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
 db = client["KRISTY"]
+gbanned = db.gban
